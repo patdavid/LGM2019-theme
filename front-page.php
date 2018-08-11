63a02182783ff8afd232a57d02ddf3d071d8860e
Pat David <patdavid@gmail.com>
2018-08-11 08:03:03 -0700

    Work on front page a bit

--- front-page.php
+++ front-page.php
@@ -30,14 +30,10 @@ Meeting
     <div class='section-para'>
         <p>
         The Libre Graphics Meeting (LGM) is an annual meeting on free and open source software for graphics.
+Since 2006 the Libre Graphics Meeting aims to attract developers, artists and professionals who use and improve free and open source software graphics applications.
         </p>
-
         <p>
-            Held yearly since 2006 the Libre Graphics Meeting aims to attract developers, artists and professionals who use and improve free and open source software graphics applications.
-        </p>
-
-        <p>
-            LGM promotes discussions amongst software developers, users, and the large intersection of those two categories. Unlike many events devoted to Free/Libre Open Source Software, LGM has always had a strong artistic focus, with designers and artists showcasing their work alongside the work of software developers.
+            LGM promotes discussions amongst software developers, users, and the large intersection of those two categories. LGM has always had a strong artistic focus, with designers and artists showcasing their work alongside the work of software developers.
         </p>
 
         <p>
