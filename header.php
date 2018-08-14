ac97ea55cc92bbb3fcc929b813fc07893ba22ebb
Pat David <patdavid@gmail.com>
2018-08-14 17:17:28 -0500

    Modify page/post header and background

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
